b3c17db9c5a73ff3b785b8eac1f0c6cf036b4e2d
Guokai <huaguokai1215@163.com>
2024-09-19 11:52:37 +0800

    0919react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Accordian from "./components/Accordian/Accordian";
 import Color from "./components/Color/Color";
 import StarRating from "./components/Star_rating";
+import ImgSlider from "./components/Img_slider";
 
 function App() {
   return (
     <div>
       {/* <Accordian /> */}
       {/* <Color /> */}
-      <StarRating noOfStars={5} />
+      {/* <StarRating noOfStars={5} /> */}
+      <ImgSlider url="https://picsum.photos/v2/list" page={1} limit={10} />
     </div>
   );
 }
--- src/components/Img_slider/index.jsx
+++ src/components/Img_slider/index.jsx
@@ -1,33 +1,105 @@
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
-import { useState } from "react";
-import "./styles.css"
+import { useState, useEffect } from "react";
+import "./styles.css";
 
-function ImgSlider({url, page, limit}) {
-    const [images, setImages] = useState([]);
-    const [curSlider, setCurSlider] = useState(0);
-    const [loading, setLoading] = useState(false);
-    const [errorMsg, setErrorMsg] = useState(null);
+function ImgSlider({ url, page = 1, limit = 10 }) {
+  const [images, setImages] = useState([]);
+  const [curSlider, setCurSlider] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-    async function fetchImage(getUrl) {
-        try {
-            setLoading(true);
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
 
-            const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
-            const data = await response.json();
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
 
-            if (data) {
-                setImages(data);
-                setLoading(false);
-            }
-        } catch(e) {
-            setErrorMsg(e.message);
-            setLoading(false);
-        }
+      if (data) {
+        setImages([...data]);
+        setLoading(false);
+      }
+    } catch (error) {
+      setErrorMsg(error.message);
+      setLoading(false);
     }
+  }
 
+  function prevHandler() {
+    setCurSlider(curSlider === 0 ? images.length - 1 : curSlider - 1);
+  }
 
+  function nextHandler() {
+    setCurSlider(curSlider === images.length - 1 ? 0 : curSlider + 1);
+  }
+  console.log(images);
 
-    if (url != '') fetchImage(url);
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
+
+  // useEffect 闭包机制
+  // 不填加依赖项，即[]时 保存当前的UI和环境状态
+  // Effect仅生效一次，在组件挂载时，后续不再生效；卸载前再进行清理
+  // 添加依赖项时, 即[curSlider]
+  // 挂载时生效；依赖项每次更新时才生效，同时会先清理上次的Effect影响；
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextHandler();
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [curSlider]);
+
+  if (loading) {
+    return <div>resource loading...</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error occurred. {errorMsg}</div>;
+  }
+
+  return (
+    <div className="slider__container">
+      <BsArrowLeftCircleFill
+        className="arrow arrow__left"
+        onClick={() => prevHandler()}
+      />
+      {images.map((imageItem, index) => {
+        return (
+          <div
+            className={
+              curSlider === index
+                ? "img_container visible"
+                : "img_container invisible"
+            }
+            key={index}
+          >
+            <img src={imageItem.download_url} alt={imageItem.url} />
+          </div>
+        );
+      })}
+      <BsArrowRightCircleFill
+        className="arrow arrow__right"
+        onClick={() => nextHandler()}
+      />
+      <span className="indicator__container">
+        {images.map((_, index) => {
+          return (
+            <div
+              className={
+                curSlider === index
+                  ? "img_indicator"
+                  : "img_indicator inactive_indicator"
+              }
+              key={index}
+              onClick={() => setCurSlider(index)}
+            />
+          );
+        })}
+      </span>
+    </div>
+  );
 }
 
 export default ImgSlider;
